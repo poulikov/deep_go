407a82e5c23f620469db60ee074107c35832a526
Sergey Poulikov <sergey@poulikov.ru>
2025-04-13 15:24:08 +0300

    using a for loop to reverse bytes instead of using math/bits package

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math/bits"
 	"testing"
 	"unsafe"
 
@@ -11,15 +10,19 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian[T ~uint16 | ~uint32 | ~uint64](number T) T {
-	switch unsafe.Sizeof(number) {
-	case 2:
-		return T(bits.ReverseBytes16(uint16(number)))
-	case 4:
-		return T(bits.ReverseBytes32(uint32(number)))
-	case 8:
-		return T(bits.ReverseBytes64(uint64(number)))
+	var (
+		res  T
+		size = int(unsafe.Sizeof(number))
+		p    = unsafe.Pointer(&number)
+		p2   = unsafe.Pointer(&res)
+	)
+
+	for i := range size / 2 {
+		*(*byte)(unsafe.Add(p2, i)) = *(*byte)(unsafe.Add(p, size-1-i))
+		*(*byte)(unsafe.Add(p2, size-1-i)) = *(*byte)(unsafe.Add(p, i))
 	}
-	panic("unreachable")
+
+	return res
 }
 
 func TestConversion(t *testing.T) {
@@ -46,6 +49,45 @@ func TestConversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestConversion64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x0000FFFF0000FFFF,
+			result: 0xFFFF0000FFFF0000,
+		},
+		"test case #5": {
+			number: 0x0102030401020304,
+			result: 0x0403020104030201,
+		},
+		"test case #6": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
 		},
 	}
 
