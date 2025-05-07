00ff0f2c596aa67bef3e0654081d44f6fb084abf
Sergey Poulikov <sergey@poulikov.ru>
2025-05-07 23:04:18 +0300

    hw9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +18,50 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+func Serialize[T any](person T) string {
+	t := reflect.TypeOf(person)
+	if t.Kind() != reflect.Struct {
+		return ""
+	}
+	v := reflect.ValueOf(person)
+	serialized := make([]string, 0, t.NumField())
+	for i := range t.NumField() {
+		fldType := t.Field(i)
+		props := strings.Split(fldType.Tag.Get("properties"), ",")
+		if props[0] == "" {
+			continue
+		}
+		omitempty := false
+		if len(props) == 2 && strings.ToLower(strings.TrimSpace(props[1])) == "omitempty" {
+			omitempty = true
+		}
+		fldVal := v.Field(i)
+		if omitempty && fldVal.IsZero() {
+			continue
+		}
+		serialized = append(serialized, fmt.Sprintf("%s=%v", props[0], fldVal))
+	}
+	return strings.Join(serialized, "\n")
+}
+
+func TestDifferentTypes(t *testing.T) {
+	tests := map[string]struct {
+		person any
+		result string
+	}{
+		"test case with channel": {
+			person: make(chan int),
+			result: "",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := Serialize(test.person)
+			assert.Equal(t, test.result, result)
+		})
+	}
+
 }
 
 func TestSerialization(t *testing.T) {
